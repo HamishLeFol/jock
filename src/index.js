b81a48984fb0e2e9fd505edd3dd5ddd8d4622f66
DevConcretize <102519658+DevConcretize@users.noreply.github.com>
2023-02-10 13:50:50 +0100

    Mise en place et entrainement
    Ajout de script pour gérer la base de React (plus JFX)
    Ajout d'un component pour s'entrainer.
    Ajout de Jest pour le test.

--- src/index.js
+++ src/index.js
@@ -11,6 +11,21 @@ root.render(
   </React.StrictMode>
 );
 
+
+class Welcome extends React.Component {
+
+  render(){
+    return <h1>Bonjour, {this.props.name}</h1>
+  }
+}
+
+
+const welcomeTest = <Welcome name="Sara"/>
+const componentTest = ReactDOM.createRoot(document.getElementById('componentTest'));
+componentTest.render(
+  welcomeTest
+);
+
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
