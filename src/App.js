0088e3afbd95fa7b74c2d07f74eadfdd8b6e741c
DevConcretize <102519658+DevConcretize@users.noreply.github.com>
2023-02-10 15:31:49 +0100

    Fix bootstrap
    l'importation des fichiers CSS se fait dans le fichier App.js avec des imports.
    à la place des <link/> dans Connexion.

--- src/App.js
+++ src/App.js
@@ -3,6 +3,16 @@ import './App.css';
 import Navbar from "./components/Navbar.js"
 import Connexion from "./components/Connexion.js"
 
+import "./assets/web/assets/mobirise-icons/mobirise-icons.css";
+import "./assets/bootstrap/css/bootstrap.min.css";
+import "./assets/bootstrap/css/bootstrap-grid.min.css";
+import "./assets/bootstrap/css/bootstrap-reboot.min.css";
+import "./assets/dropdown/css/style.css";
+import "./assets/socicon/css/styles.css";
+import "./assets/theme/css/style.css";
+import "./assets/mobirise/css/mbr-additional.css"
+
+
 
 function App() {    
     return (
